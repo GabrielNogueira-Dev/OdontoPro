dbf0369126c7e115f1323ab350629ab8c89869de
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2026-09-23 16:24:02 +0100

    burguer mobile

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,16 +1,53 @@
 import Link from "next/link";
-
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export function Header() {
   return (
-   <header className= "fixed top-0 left-0 right-0 z-999">
-    <div className="">
-        <Link href="/">
-            OdontoPro
+   <header className= "fixed top-0 left-0 right-0 z-999 py-4 px-6 bg-white">
+    <div className="container mx-auto flex items-center justify-between">
+        <Link href="/"
+        className= "font-bold text-zinc-900 text-3xl">
+            Odonto<span className="text-emerald-500 ">PRO</span>
         </Link>
-        <nav>
+        <nav className="hidden md:flex items-center">
             <a href="#">Profissionais</a>
         </nav>
+
+    <Sheet>
+        <SheetTrigger asChild className="md:hidden cursor-pointer">
+            <Button className= "text-black hover:bg-transparent"
+                    variant ="ghost" size="icon">
+                <Menu className="w-6 h-6 "/>
+            </Button>
+        </SheetTrigger>
+
+        <SheetContent side="right" className="w-60 sm:w-64 z-9999 transition-all duration-100 ease-in">
+            <SheetTitle>Menu</SheetTitle>
+            <SheetHeader></SheetHeader>
+
+            <SheetDescription>
+                Veja nossos links
+            </SheetDescription>
+
+            <nav >
+                <a href="#">Profissionais</a>
+            </nav>
+
+        </SheetContent>
+
+    </Sheet>
+
     </div>
    </header>
   );
